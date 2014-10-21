d317e2bb35ecee0d8f44aa63d35cf2d8cda9ee30
clovisf <clovisf@gmail.com>
2014-10-21 03:30:51 -0700

    Initial release of the delay() substitute sketch
    First commit of the delay() substitute sketch, which takes advantage of
    the micros() function and allows the Arduino runtime to be fully
    utilized (instead of being lost, as occurs with the delay() function).

---
+++ substitute_for_delay_function_raw_v01.ino
@@ -0,0 +1,64 @@
+
+/*************************************************** 
+ * This code greatly substitutes the delay() function of Arduino.
+ * 
+ * It takes advantage of the micros() function, which returns the number
+ * of microseconds since the Arduino has been reset for the last time. 
+ * 
+ * Notice that the micros() function will overflow every 70 minutes,
+ * according to this reference: http://arduino.cc/en/reference/micros ; 
+ * I have not tested the effect of this reset on this code yet (as by
+ * 10/21/2014).
+ * 
+ * Written by Clovis Fritzen (clovisf AT gmail DOT com ) in 10/21/2014.
+ * http://embedded-clovis.blogspot.ca/
+ * http://www.twitter.com/clovisf  
+ * 
+ * BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// Variables used on this code
+unsigned long time;
+unsigned long previousTime;
+boolean enterFunction= true;
+//-----------------------
+
+
+
+void setup() {
+    
+}
+
+void loop() {
+  time = micros();
+  if (enterFunction == true){
+    previousTime= time;
+    Serial.println(previousTime); // for debugging
+
+    // Start your code below 
+    //-----------------------
+
+
+
+
+    //-----------------------
+    // End of your code
+  }
+  
+  // The DELAY time is adjusted in the constant below >> 
+  if (time - previousTime < 999990){ // 1 million microsencods= 1 second delay
+    /* I have actually used 0.999990 seconds, in a trial to compensate the time that
+       this IF function takes to be executed. this is really a point that
+       need improvement in my code */   
+    enterFunction= false;
+  } 
+  else {
+    enterFunction= true;
+  }
+
+
+
+
+
+}
+
